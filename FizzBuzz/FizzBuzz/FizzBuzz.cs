59c73418f3d59e86cf4359c0de4cfd0ff722f03d
Wei Qin <qin.we@husky.neu.edu>
2015-09-29 17:16:55 -0400

    First commit

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -4,11 +4,36 @@
 	{
 		public string RunFizzBuzz(int number)
 		{
-			string result = number.ToString();
+            string result = number.ToString();
 
-			// your solution/implementation should go in here
-
-			return result;
+            // your solution/implementation should go in here
+            if (number == 0)
+            {
+                result = "0";
+            }
+            else if (number == 1)
+            {
+                result = number.ToString();
+            }
+            else
+            {
+                string Fizz = "Fizz";
+                string Buzz = "Buzz";
+                string FizzBuzz = "FizzBuzz";
+                if (number % 3 == 0 && number % 5 != 0)
+                {
+                    result = Fizz;
+                }
+                else if (number % 5 == 0 && number % 3 != 0)
+                {
+                    result = Buzz;
+                }
+                else if (number % 5 == 0 && number % 3 == 0)
+                {
+                    result = FizzBuzz;
+                }
+            }
+          return result;
 		}
 	}
 }
