6dd8983b196cbb887e85fb4446650863df11a5c1
Wei Qin <qin.we@husky.neu.edu>
2015-10-04 03:01:15 -0400

    PA

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -12,26 +12,87 @@ namespace TrianglePatterns
 			DisplayPatternD();
 
 			Console.ReadLine();
-		}
+            
+        }
 
 		static void DisplayPatternA()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            int i, j, k;
+            Console.WriteLine("Patten A");
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= 10 - i; j++)
+                {
+                    //Console.Write("");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+    }
 
 		static void DisplayPatternB()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Patten B");
+            int i, j, k;
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= 10 - i; j++)
+                {
+                    // Console.Write(" ");
+                }
+                for (k = 1; k <= j; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
 
-		static void DisplayPatternC()
-		{
-			// your implementation here
-		}
+        static void DisplayPatternC()
+        {
+            // your implementation here
+            Console.WriteLine("Patten C");
+            int i, j, k;
+            for (i = 10; i >= 1; i--) 
+            {
+                for (j = 1; j <= 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
 
 		static void DisplayPatternD()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Patten D");
+            int i, j, k;
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= 10 - i; j++)
+                {
+                    //Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
 	}
 }
