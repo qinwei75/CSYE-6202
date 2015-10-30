bf212be2ea74ef8f22421b8e90ca62135ef57f92
Wei Qin <qin.we@husky.neu.edu>
2015-10-29 22:33:39 -0400

    Program

--- Carbon Footprint/Carbon Footprint/Program.cs
+++ Carbon Footprint/Carbon Footprint/Program.cs
@@ -10,7 +10,27 @@ namespace Carbon_Footprint
     {
         static void Main(string[] args)
         {
-
+            double totalCost = 0;
+            double sum = 0;
+            // your implementation here
+            while (true)
+            {
+                Console.Write("Type: Car, Bicycle, Building");
+                Console.Write("Please enter a type, Q/q to quit:");
+                string userInput = Console.ReadLine();
+                if (Business.ItemHelper.ValidateItemType(userInput))
+                {
+                    Console.WriteLine("Application terminated");
+                    Console.ReadLine();
+                    System.Environment.Exit(1);
+                }
+                //check ItemType input
+                else if (Business.ItemHelper.ValidateItemType(userInput))
+                {
+                    Console.WriteLine(userInput);
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
---
+++ Carbon Footprint/business/Properties/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon_Footprint
+{
+    public class Item
+    {
+        public enum ItemType
+        {
+            Bicycle,
+            Car,
+            Building
+        }
+        private ItemType typeName { get; set; }//Car, Bike, Building
+        private double carbonOutput { get; set; }
+        public abstract double CalculateCarbonFootprints();
+    }
+}
