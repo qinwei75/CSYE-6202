a25b6d75076f9b77d38858228d6f3e8a0aebd4ad
Wei Qin <qin.we@husky.neu.edu>
2015-10-29 21:16:03 -0400

    ok

---
+++ Carbon Footprint/Carbon Footprint/ItemHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon_Footprint
+{
+    public class ItemHelper
+    {
+        public static bool ValidateItemType(string enteredType, ref Program.ItemType itemType)
+        {
+            if (enteredType == "")
+            {
+                return false;
+            }
+            if (enteredType == "Bicycle")
+            {
+                itemType = Program.ItemType.Bicycle;
+                return true;
+            }
+            else if (enteredType == "Car")
+            {
+                itemType = Program.ItemType.Car;
+                return true;
+            }
+            else if (enteredType == "Building")
+            {
+                itemType = Program.ItemType.Building;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
--- Carbon Footprint/Carbon Footprint/Program.cs
+++ Carbon Footprint/Carbon Footprint/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Carbon_Footprint
 {
-    class Program
+    public class Program
     {
         public enum ItemType
         {
